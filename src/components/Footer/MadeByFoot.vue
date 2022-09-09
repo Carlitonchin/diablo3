4b58e2f6654cdca37bd91268c6fb0e21fc1bfc7d
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-09 15:19:40 -0300

    create footer component with some childs

---
+++ src/components/Footer/MadeByFoot.vue
@@ -0,0 +1,8 @@
+<template>
+    <p class="text-muted m-0 pt-1 pl-3 text-left text-sm-right">
+      <small>
+        Made by
+        <a href="https://twitter.com/baumannzone" target="_blank">Carlos Arrieta</a>
+      </small>
+    </p>
+  </template>
