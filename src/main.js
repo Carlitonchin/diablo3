854438a11625ace20cedddc72d105cfecb7a0597
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-09 16:58:28 -0300

    third part librarys moved to plugins folder

--- src/main.js
+++ src/main.js
@@ -1,33 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { BootstrapVue3 } from 'bootstrap-vue-3'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+import BootstrapVue3 from './plugins/bootstrap'
+import FontAwesomeIcon from './plugins/fontawesome'
 import './assets/css/main.styl'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-// Iconos de tipo "Solid"
-import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
-// Iconos de tipo "Brand" (marcas o logos de empresas)
-import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
-// El componente que vamos a utilizar
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(
-    faSkull,
-    faCrown,
-    faDungeon,
-    faHatWizard,
-    faHammer,
-    faGem,
-    faVuejs,
-    faBootstrap,
-    faFontAwesome,
-    faGithub,
-    faBattleNet
-  )
 
 let app = createApp(App)
 
