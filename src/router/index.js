6ca90b5a5161fa09d735be32bfe4fa85caad859b
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-09 18:25:30 -0300

    change routes file for lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import IndexView from '../views/Home/Index.vue'
-import AboutView from '../views/About.vue'
-import ProfileView from '../views/Profile.vue'
+
+const routerOptions = [
+  { path: '/', name: 'Home' },
+  { path: '/profile?region=:region&profile=:battleTag', name: 'Profile' },
+  { path: '/hero?region=:region&profile=:battleTag&hero=:heroId', name: 'Hero' },
+  { path: '/about', name: 'About' },
+  { path: '/error', name: 'Error' },
+  { path: '/:pathMatch(.*)*', redirect: { name: 'Home' } }
+]
+
+const routes = routerOptions.map(r=>{
+  return {
+    ...r,
+    component: ()=>import(`../views/${r.name}/Index.vue`)
+  }
+})
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: IndexView
-    },
-    {
-      path:"/about",
-      name: "About",
-      component:AboutView
-    },
-    {
-      path:"/profile",
-      name: "Profile",
-      component:ProfileView
-    }
-  ]
+  routes
 })
+
 
 export default router
