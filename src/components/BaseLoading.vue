63474360d6a9ac170abf900b014be9e78be1fd6a
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-09 12:53:20 -0300

    add loading component

---
+++ src/components/BaseLoading.vue
@@ -0,0 +1,48 @@
+<script setup>
+    import { defineProps } from 'vue';
+    const {variant, type} = defineProps({
+        //prop
+        variant:{
+            type:String,
+            default:"info",
+            validator: value=>{
+                return [
+                    'primary',
+                    'secondary',
+                    'danger',
+                    'warning',
+                    'success',
+                    'info',
+                    'light',
+                    'dark'
+                    ].includes(value)
+            }
+        },
+        //prop
+        type:{
+            type:String,
+            default:"border",
+            validator: value=>{
+                return ['border', 'grow'].includes(value)
+            }
+        }
+    })
+</script>
+
+<template>
+    <!--Clases CSS de Bootstrap-->
+    <div class="loading-wrapper text-center py-5">
+      <div class="spinner-wrapper w-100">
+        <!--Componente Spinner de Bootstrap 🔄-->
+        <b-spinner class="spinner" label="Loading..." :type="type" :variant="variant"/>
+      </div>
+      <h1 class="my-5">Loading...</h1>
+    </div>
+  </template>
+
+<style lang="stylus" scoped>
+    .spinner-wrapper
+      .spinner
+        width 4rem
+        height 4rem
+  </style>
