778695cdfb9878c5a2722dd0c83bc9383f2359ca
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-09 13:03:39 -0300

    add style to index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,13 +3,3 @@
     <h1>Home Page</h1>
   </div>
   </template>
-
-<style lang="stylus">
-  #app
-    padding 60px 0
-    font-family 'Avenir', Helvetica, Arial, sans-serif
-    -webkit-font-smoothing antialiased
-    -moz-osx-font-smoothing grayscale
-    color #ffffff // Le ponemos un color de letra blanco para que resalte
-    background-color #15202b // Le cambiamos el color de fondo por un azul marino oscuro
-</style>
