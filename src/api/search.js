b52e349561ab1c6d12e537242302e9c46fc2a3b2
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-09 18:51:19 -0300

    add api get request for profile

---
+++ src/api/search.js
@@ -0,0 +1,30 @@
+import { get } from 'axios'
+import store from '../store'
+import { locales } from '../utils/regions'
+
+const PROTOCOL = "https://"
+const HOST = '.api.blizzard.com/'
+
+/**
+ * Returns the specified account profile.
+ * GET – /d3/profile/{account}
+ * Los parámetros que hemos obtenido a través de la URL
+ *  - @param region {String}
+ *  - @param account {String}
+ * @returns {Promise}
+ */
+ function getApiAccount ({ region, account }) {
+    const resourse = `d3/profile/${account}/`
+    const API_URL = `${PROTOCOL}${region}${HOST}${resource}`
+
+    const params = {
+        access_token: store.state.oauth.accessToken,
+        locale
+      }
+
+    return get(API_URL, {params})
+ }
+
+ export {
+    getApiAccount
+ }
