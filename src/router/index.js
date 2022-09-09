9fa15a434f541e42647b81c297bc513ad86a7097
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-09 17:37:21 -0300

    add profile view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import IndexView from '../views/Home/Index.vue'
 import AboutView from '../views/About.vue'
+import ProfileView from '../views/Profile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path:"/about",
       name: "About",
       component:AboutView
+    },
+    {
+      path:"/profile",
+      name: "Profile",
+      component:ProfileView
     }
   ]
 })
