5cbf372712007544abb4595f6072ebb446cc100f
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-09 19:00:16 -0300

    add error vuex module

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
 import {createStore} from 'vuex'
 import oauth from './modules/oauth'
 import loading from './modules/loading'
-
+import error from './modules/error'
 
 const store = createStore({
     modules:{
         oauth,
-        loading
+        loading,
+        error
     }
     ,
     actions:{
