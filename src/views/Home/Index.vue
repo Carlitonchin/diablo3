d498b773c990264b6d47b08f3e557036a7deaa71
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-09 17:34:26 -0300

    add Home form to index page

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,9 +1,12 @@
 <script setup>
 import HomeTitle from '../../components/Home/HomeTitle.vue';
+import HomeForm from '../../components/Home/HomeForm.vue';
 </script>
 
 <template>
     <div class="home">
       <HomeTitle/>
+
+      <HomeForm/>
   </div>
   </template>
