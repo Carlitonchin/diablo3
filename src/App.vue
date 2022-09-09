599e6fcc644bcc28df6221ecbe9dae0af1a61f71
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-09 13:03:13 -0300

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,23 @@ init()
 </script>
 
 <template>
-  <h1>Hola</h1>
+  <!-- Esta parte no la hemos tocado aún -->
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
+
+<style lang="stylus">
+  #app
+    padding 60px 0
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    color #ffffff // Le ponemos un color de letra blanco para que resalte
+    background-color #15202b // Le cambiamos el color de fondo por un azul marino oscuro
+</style>
