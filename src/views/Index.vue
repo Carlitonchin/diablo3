c3af4e1c36f6e7578ad9875a025c8353945b3592
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-09 13:03:28 -0300

    update index page

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="hello">
-      <b-button>Just a Button!</b-button>
-    </div>
+    <div class="home">
+    <h1>Home Page</h1>
+  </div>
   </template>
+
+<style lang="stylus">
+  #app
+    padding 60px 0
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    color #ffffff // Le ponemos un color de letra blanco para que resalte
+    background-color #15202b // Le cambiamos el color de fondo por un azul marino oscuro
+</style>
